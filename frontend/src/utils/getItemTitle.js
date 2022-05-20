0b875800d034708f08e0f56a1b52471a507ac5c1
Guilherme Viana <guilhermehviana01@gmail.com>
2022-05-20 18:56:42 -0300

    refactor: refatora função que captaliza titulo com nome do produto

--- frontend/src/utils/getItemTitle.js
+++ frontend/src/utils/getItemTitle.js
@@ -1,8 +1,7 @@
 const getItemTitle = (title) => {
   const firstStr = title[0].toUpperCase();
   const othersStr = title.slice(1, title.length).toLowerCase();
-  const captalized = othersStr.includes('de')
-    ? firstStr + othersStr : (firstStr + othersStr).replace(' ', ' de ');
+  const captalized = firstStr + othersStr;
 
   return captalized;
 };
